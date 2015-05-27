4a370a808fb970c52da69c8540ad08cea9cf9e41
David J. Mendez <mendezdavey@gmail.com>
2015-05-26 21:20:50 -0600

    Small change to style and web_server_tut

--- web_server_tut.php
+++ web_server_tut.php
@@ -1,3 +1,4 @@
+
 <?php 
 	define("TITLE", "Web Server Tutorial | David Mendez");
 	include('includes/php/header.php'); 
@@ -35,6 +36,33 @@
 	<p>
 		Here is a detailed guide: <a href="http://www.servermom.org/basic-ubuntu-setup-before-building-a-working-server/85/">http://www.servermom.org/basic-ubuntu-setup-before-building-a-working-server/85/</a>
 	</p>
+  <h2>Step 3: Install SSH</h2>
+  <p>Downloading openssh-server will allow you to remotely control your computer
+  by connecting to it via ssh. For example, I connect to my home server by typing its
+ ip address in a terminal(linux or mac) or using putty(windows). More on this in step 4.</p>
+  <p>a) Run the command below to install openssh-server:</p>
+  <p>sudo apt-get install openssh-server</p>
+  <p>b) You can make aditional changes by editing the ssh config file:</p>
+  <p class="gray-highlight">sudo nano /etc/ssh/sshd_config</p>
+  <p>c) Restart openssh-server:</p>
+  <p class="gray-highlight">sudo /etc/init.d/ssh restart</p>
+  <p>All done!</p>
+  
+  <h2>Step 4: Remotely connect to your computer via terminal(linux or mac) 
+   or <a href="http://putty.org">putty</a>(windows).</h2>
+  <p>Linux computers and mac's have built in tools to connect via ssh. if you are using
+   a microsoft windows machine download putty. To download putty search for it 
+ on google.com</p>
+  <p>a) Start by typing ifconfig on your server and look for your machine's ip address.</p>
+  <p>You should look for something like this next to eth0, eth1, depending on your 
+  particular setup:</p>
+  <p>eth0</p>
+  <p>inet addr: 192.168.1.100. This is your ip adress</p>
+  <p>Go to your client computer and launch a terminal window or putty(windows).</p>
+  <p>Type in the ip adress of your server in terminal:</p>
+  <p>ssh mike@192.168.1.100</p>
+  <p>Similar execution in putty.</p>
+  <p>All done! You should be able to connect to your server via ssh.</p>
 </div>
 
 <!-- END content -->
